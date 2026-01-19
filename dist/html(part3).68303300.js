ae0ae63bb9fccb2b5f160469e979951f136b315a
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-01-19 15:17:42 +0500

    add

---
+++ dist/html(part3).68303300.js
@@ -0,0 +1,2 @@
+const collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
+//# sourceMappingURL=html(part3).68303300.js.map
---
+++ dist/html(part3).72be8890.js
@@ -0,0 +1,6 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item)=>item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded");
+    }));
+
+//# sourceMappingURL=html(part3).72be8890.js.map
